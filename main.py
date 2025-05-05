7a8fde210d90cef3d03d7e3bc890fc01899ae4ab
Eheath04 <eheath@trinity.edu>
2025-05-05 12:58:06 -0500

    Question #3 with basic architecture

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers import Dense
 from utils import preprocess
-
+from tensorflow.keras.layers import Dropout
 
 try:
     df = pd.read_csv('diabetes.csv')
@@ -24,9 +24,22 @@ except:
 df = preprocess(df)
 
 # Split the data into a training and testing set
+X=df.drop(columns=['Outcome'])
+y=df['Outcome']
 
+x_train,x_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
+print("training set size:", x_train.shape)
 
 # Build MLP
-model = Sequential()
-
+model = Sequential(
+        [Dense(32,activation='relu',name='layer1',input_shape=(X.shape[1],)),
+         Dense(16,activation='relu',name='layer2'),
+         Dropout(0.3),
+         Dense(8,name='layer3'),
+         Dropout(0.2),
+         Dense(1,activation='sigmoid',name='output'),]
+        )
+model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
+model.fit(x_train,y_train,epochs=100,batch_size=16,validation_data=(x_test,y_test))
+model.evaluate(x_test,y_test)
 # Results - Accuracy
